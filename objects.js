c01a7f995ac7e780367316e875a94acde2e38aa7
Akhmad Faris Faizaki <faris.faizaki@gmail.com>
2017-08-29 07:54:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,6 @@ function updatePlaylist(playlist,artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist,artistName){
-  Object.assign({},playlist);
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
